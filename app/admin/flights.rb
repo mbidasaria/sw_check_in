b65405fccafc70b6633e33f9ffdb10f0f94c5a6d
Raghu Betina <raghu@firstdraft.com>
2019-11-21 20:29:06 +0000

    Whitelist columns in Flight for ActiveAdmin

--- app/admin/flights.rb
+++ app/admin/flights.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Flight do
+
+ permit_params :description, :departs_at, :user_id, :ten_minute_message_sent, :five_minute_message_sent, :one_minute_message_sent
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
